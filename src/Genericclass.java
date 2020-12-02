c6b719492409d3842b4840c0b4f97914fdade8b3
MURA199918 <muralisaranr18@gmail.com>
2020-12-02 15:40:20 +0530

    Added UC2 to check maximum Float

--- src/Genericclass.java
+++ src/Genericclass.java
@@ -12,8 +12,14 @@ public class Genericclass {
 
     public static void main(String[] args) {
         System.out.println(".......Generics practices.......");
+        System.out.println("Finding maximum for Integers");
         System.out.println("Maximum of 3,2,1 is: "+maximum(3,2,1));
         System.out.println("Maximum of 4,5,3 is: "+maximum(4,5,3));
         System.out.println("Maximum of 4,5,6 is: "+maximum(4,5,6));
+
+        System.out.println("Finding maximum for Float");
+        System.out.println("Maximum of 3.1,2.1,1.1 is: "+maximum(3.1,2.1,1.1));
+        System.out.println("Maximum of 4.5,5.5,3.5 is: "+maximum(4.5,5.5,3.5));
+        System.out.println("Maximum of 4.5,5.5,6.5 is: "+maximum(4.5,5.5,6.5));
     }
 }
