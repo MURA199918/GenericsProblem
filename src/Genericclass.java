c8c7f272f8378c87febe94c88171d7cf3e2a3fbb
MURA199918 <muralisaranr18@gmail.com>
2020-12-02 16:15:55 +0530

    Added UC5 to write printmax generic method internally

--- src/Genericclass.java
+++ src/Genericclass.java
@@ -7,7 +7,13 @@ public class Genericclass {
             max=y;
         if(z.compareTo(max)>0)
             max=z;
+        printmax(x,y,z,max);
         return max;
+    }
+    public static <T extends Comparable<T>> T printmax(T x,T y,T z,T max){
+        T maximum =max;
+        System.out.println("Maximum of x,y,z is: "+maximum);
+        return maximum;
     }
 
     public static <T extends Comparable<T>> T maximumoflist(T[] list){
@@ -23,19 +29,22 @@ public class Genericclass {
     public static void main(String[] args) {
         System.out.println(".......Generics practices.......");
         System.out.println("Finding maximum for Integers");
-        System.out.println("Maximum of 3,2,1 is: "+maximumof3(3,2,1));
-        System.out.println("Maximum of 4,5,3 is: "+maximumof3(4,5,3));
-        System.out.println("Maximum of 4,5,6 is: "+maximumof3(4,5,6));
+        //System.out.println("Maximum of 3,2,1 is: "+maximumof3(3,2,1));
+        //System.out.println("Maximum of 4,5,3 is: "+maximumof3(4,5,3));
+        //System.out.println("Maximum of 4,5,6 is: "+maximumof3(4,5,6));
+        System.out.println(maximumof3(3,2,1));
 
         System.out.println("Finding maximum for Float");
-        System.out.println("Maximum of 3.1,2.1,1.1 is: "+maximumof3(3.1,2.1,1.1));
-        System.out.println("Maximum of 4.5,5.5,3.5 is: "+maximumof3(4.5,5.5,3.5));
-        System.out.println("Maximum of 4.5,5.5,6.5 is: "+maximumof3(4.5,5.5,6.5));
+        //System.out.println("Maximum of 3.1,2.1,1.1 is: "+maximumof3(3.1,2.1,1.1));
+        //System.out.println("Maximum of 4.5,5.5,3.5 is: "+maximumof3(4.5,5.5,3.5));
+        //System.out.println("Maximum of 4.5,5.5,6.5 is: "+maximumof3(4.5,5.5,6.5));
+        System.out.println(maximumof3(3.1,2.1,1.1));
 
         System.out.println("Finding maximum for Strings");
-        System.out.println("Maximum of Apple, Peach, Banana is: "+maximumof3("Apple","Peach","Banana"));
-        System.out.println("Maximum of Peach, Banana, Apple is: "+maximumof3("Apple","Banana","Peach"));
-        System.out.println("Maximum of Apple, Peach, Banana is: "+maximumof3("Banana","Apple","Peach"));
+        //System.out.println("Maximum of Apple, Peach, Banana is: "+maximumof3("Apple","Peach","Banana"));
+        //System.out.println("Maximum of Peach, Banana, Apple is: "+maximumof3("Apple","Banana","Peach"));
+        //System.out.println("Maximum of Apple, Peach, Banana is: "+maximumof3("Banana","Apple","Peach"));
+        System.out.println(maximumof3("Banana","Apple","Peach"));
 
 
         System.out.println("Finding maximum of more than 3 elements");
